7bd8276065bff79b8e3b989973be020e91de7dad
Pandu Satria Nur Ananda <pandsatrianura@gmail.com>
2026-01-27 21:35:38 +0700

    feat: update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -13,9 +13,8 @@ func InitConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			log.Println("Warning: .env file not found, falling back to environment variables")
-		} else {
-			log.Fatalf("Fatal error reading config file: %v", err)
 		}
 	}
+
 	viper.AutomaticEnv()
 }
