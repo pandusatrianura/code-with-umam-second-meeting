2fee5d0865e5bcba24b83f56923967369dbf3528
Pandu Satria Nur Ananda <pandsatrianura@gmail.com>
2026-01-28 17:22:39 +0700

    add: https for swagger @host

--- docs/docs.go
+++ docs/docs.go
@@ -515,7 +515,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "railway.pandusatrianura.cloud",
+	Host:             "https://railway.pandusatrianura.cloud",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Kasir API",
